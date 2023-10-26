919c60467299bb2eb8ab3c0eb24ee1a3725057a6
Joel1414 <144406965+Joel1414@users.noreply.github.com>
2023-10-26 23:45:57 +1000

    Add files via upload

--- components/OutfitItem.js
+++ components/OutfitItem.js
@@ -2,23 +2,24 @@ import React from 'react';
 import { View, Text, Image, TouchableOpacity } from 'react-native';
 import styles from '../styles'; 
 
-const OutfitItem = ({ item, onRefresh, onFavorite, onInfo }) => {
+
+const OutfitItem = ({ itemName, itemImage, onRefresh, onFavorite, onInfo }) => {
     return (
         <View style={styles.outfitItemContainer}>
-            <Image source={{ uri: item.base64Data }} style={styles.outfitItemImage} />
+            <Image source={itemImage} style={styles.outfitItemImage} />
             <View style={styles.outfitItemInnerContainer}>
-                <Text style={styles.outfitItemText}>{item.name}</Text>
-                <View style={styles.outfitIconContainer}>
-                    <TouchableOpacity onPress={onRefresh}>
-                        <Image source={require('../icons/Icon_Refresh.png')} style={styles.outfitItemInfoIcon} />
-                    </TouchableOpacity>
-                    <TouchableOpacity onPress={onFavorite}>
-                        <Image source={require('../icons/Favorite.png')} style={styles.outfitItemInfoIcon} />
-                    </TouchableOpacity>
-                    <TouchableOpacity onPress={onInfo}>
-                        <Image source={require('../icons/Information.png')} style={styles.outfitItemInfoIcon} />
-                    </TouchableOpacity>
-                </View>
+            <Text style={styles.outfitItemText}>{itemName}</Text>
+            <View style={styles.outfitIconContainer}>
+            <TouchableOpacity onPress={onRefresh}>
+                <Image source={require('../icons/Icon_Refresh.png')} style={styles.outfitItemInfoIcon} />
+            </TouchableOpacity>
+            <TouchableOpacity onPress={onFavorite}>
+                <Image source={require('../icons/Favorite.png')} style={styles.outfitItemInfoIcon} />
+            </TouchableOpacity>
+            <TouchableOpacity onPress={onInfo}>
+                <Image source={require('../icons/Information.png')} style={styles.outfitItemInfoIcon} />
+            </TouchableOpacity>
+            </View>
             </View>
         </View>
     );
